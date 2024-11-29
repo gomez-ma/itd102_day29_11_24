289013ab7b5e3c2d0d69f6e92bf2baf90cc46362
gomez-ma <komez@live.com>
2024-11-29 12:21:04 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.APP_PORT || 5000;
+
+app.get('/', (req, res) => { 
+    res.send('Home');
+});
+
+app.get('/about', (req, res) => { 
+    res.send('About');
+});
+
+app.listen(PORT, () => console.log('Server is running ' + PORT));
